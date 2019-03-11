f460981485a086741d2b88fd875098694a08b274
Jesus Estevez <jecaestevez@outlook.com>
2019-03-12 00:29:52 +0100

    Initial commit using the basic asp dotnet core 2.2 Web template dotnet new webapp

---
+++ 01_Basic/Swagger_Basic_Demo_Jecaestevez/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Swagger_Basic_Demo_Jecaestevez.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
